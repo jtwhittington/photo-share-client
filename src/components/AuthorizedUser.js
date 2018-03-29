2e245b0c79f398dc7f73bb650dfb842dcb7ac3a5
MoonTahoe <alex@moonhighway.com>
2018-03-29 16:20:13 -0700

    Adding the React Router

---
+++ src/components/AuthorizedUser.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+
+class AuthorizedUser extends Component {
+
+    render() {
+        console.log(this.props)
+        return <button>Sign In with Github</button>
+    }
+
+}
+
+export default withRouter(AuthorizedUser) 

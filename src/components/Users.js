3cdabbb519dff61d1ff4271ed24ba12a570f852f
MoonTahoe <alex@moonhighway.com>
2018-03-30 11:48:06 -0700

    Incorporating the UserList UI Component

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { gql } from 'apollo-boost'
-import { Query, Mutation } from 'react-apollo'
+import { Query } from 'react-apollo'
+import { UserList } from './ui'
 
 export const ALL_USERS_QUERY = gql`
     query allUsers {
@@ -51,38 +52,8 @@ const updateLocalCache = (cache, { data }) => {
 
 const Users = () => 
     <Query query={ALL_USERS_QUERY}>
-        {({data, loading }) => loading ?
-            <p>loading users...</p> :
-            <div>
-                <p>{data.totalUsers} Users</p>
-                <ul>
-                    {data.allUsers.map(user => 
-                        <li key={user.id}>
-                        <img src={user.avatar} 
-                                width={48} 
-                                height={48} 
-                                alt="" />
-                        {user.name}
-                        </li>
-                    )}
-                </ul>
-                <Mutation mutation={ADD_FAKE_USERS_MUTATION}>
-                    {addFakeUsers => {
-                        let count = 1
-                        return (
-                            <div>
-                                <input type="Number" 
-                                    defaultValue={1} 
-                                    onChange={({target}) => count = target.value} />
-                                <button onClick={() => addFakeUsers({ variables: {count}})}>
-                                    Add Fake Users
-                                </button>
-                            </div>
-                        )
-                    }}
-                </Mutation>
-            </div>
-            
+        {({data, loading }) => 
+            <UserList users={data.allUsers} loading={loading} />
         }
     </Query>   
 

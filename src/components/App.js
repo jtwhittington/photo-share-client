c76cbd62071efd3d9b6ac54ae80e88a0c4b7dc17
MoonTahoe <alex@moonhighway.com>
2018-03-30 15:32:11 -0700

    Adding Photo Subscriptions

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import { gql } from 'apollo-boost'
 import { withApollo } from 'react-apollo'
 import AuthorizedUser from './AuthorizedUser'
 import PostPhoto from './PostPhoto'
-import Photos from './Photos'
+import Photos, { LIST_PHOTOS } from './Photos'
 import { UserInterface } from './ui'
 
 const LISTEN_FOR_USERS = gql`
@@ -14,6 +14,21 @@ const LISTEN_FOR_USERS = gql`
             id
             name
             avatar
+        }
+    }
+`
+
+const LISTEN_FOR_PHOTOS = gql`
+    subscription {
+        newPhoto {
+            id
+            name
+            url
+            created
+            postedBy {
+                name
+                avatar
+            }
         }
     }
 `
@@ -27,6 +42,7 @@ class App extends Component {
 
     componentDidMount() {
         this.subscribeToUsers(this.props.client)
+        this.subscribeToPhotos(this.props.client)
     }
 
     subscribeToUsers(client) {
@@ -41,6 +57,23 @@ class App extends Component {
                     allUsers: [
                         ...allUsers,
                         data.newUser
+                    ]
+                }
+            })
+        })
+    }
+
+    subscribeToPhotos(client) {
+        client.subscribe({
+            query: LISTEN_FOR_PHOTOS
+        }).subscribe(({data}) => {
+            const { allPhotos } = client.readQuery({ query: LIST_PHOTOS })
+            client.writeQuery({
+                query: LIST_PHOTOS,
+                data: {
+                    allPhotos: [
+                        data.newPhoto,
+                        ...allPhotos
                     ]
                 }
             })

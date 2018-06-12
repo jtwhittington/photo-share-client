f684cb59ea749f38631403997560740d29dc25c2
Jonathan Whittington <jonathantwhittington@gmail.com>
2018-06-12 09:43:52 -0700

    step 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log(`TODO: Build PhotoShare Client App`)
+import {request} from 'graphql-request'
+
+const query = `{ allPhotos { name }}`
+const url   = 'http://localhost:4000/graphql'
+
+request(url, query)
+  .then(console.log)
+
+
+// const options = {
+//   method: 'POST',
+//   headers: {'Content-Type': 'application/json'},
+//   body: JSON.stringify({query}),
+// }
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(({data}) => `<p>Photos: ${data.allPhotos.map(photo=>photo.name)}</p>`)
+//   .then(text => document.body.innerHTML = text)
+//   .catch(console.error)
